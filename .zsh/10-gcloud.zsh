036e4c07781083a39554a6c534e8b8fe30920146
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2024-02-19 13:37:48 +0900

    install gcloud

---
+++ .zsh/10-gcloud.zsh
@@ -0,0 +1,2 @@
+zinit ice wait lucid as"program" pick"*/bin/*" extract atload"source */completion.zsh.inc"
+zinit snippet https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz
