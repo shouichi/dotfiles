8942037c7cd8ff7c213ad011e788f4f8fd21c05f
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2024-02-27 21:45:33 +0900

    zsh: insert last word by ctrl+] (#20)

--- .zsh/20-history.zsh
+++ .zsh/20-history.zsh
@@ -30,3 +30,8 @@ zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
 bindkey "^P" history-beginning-search-backward-end
 bindkey "^N" history-beginning-search-forward-end
+
+# insert last word by ctrl+]
+autoload -Uz smart-insert-last-word
+zle -N insert-last-word smart-insert-last-word
+bindkey "^]" insert-last-word
