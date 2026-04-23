2ebc61bbbdfa8fec9201f6b7528b16d2bdbb331f
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2026-04-23 20:26:39 +0900

    fix(zsh): fix gcloud installation for mac

--- .zsh/10-gcloud.zsh
+++ .zsh/10-gcloud.zsh
@@ -1,2 +1,6 @@
+local _gcloud_os _gcloud_arch
+[[ "$OSTYPE" == "darwin"* ]] && _gcloud_os="darwin" || _gcloud_os="linux"
+[[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]] && _gcloud_arch="arm" || _gcloud_arch="x86_64"
+
 zinit ice wait lucid as"program" pick"*/bin/*" extract atload"source */completion.zsh.inc"
-zinit snippet https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-464.0.0-linux-x86_64.tar.gz
+zinit snippet https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-565.0.0-${_gcloud_os}-${_gcloud_arch}.tar.gz
--- .zsh/10-mise.zsh
+++ .zsh/10-mise.zsh
@@ -1,6 +1,7 @@
 local _mise_os _mise_arch
 [[ "$OSTYPE" == "darwin"* ]] && _mise_os="macos" || _mise_os="linux"
 [[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]] && _mise_arch="arm64" || _mise_arch="x64"
+
 zinit ice from"gh-r" as"program" bpick"*${_mise_os}-${_mise_arch}.tar.gz" pick"mise/bin/mise"
 zinit light jdx/mise
 
