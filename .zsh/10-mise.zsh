720cd2803eaaaaa2fdf0ceb92e9713a47d317c32
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2026-04-23 10:41:03 +0900

    fix(zsh): fix mise installation for mac

--- .zsh/10-mise.zsh
+++ .zsh/10-mise.zsh
@@ -1,4 +1,7 @@
-zinit ice from"gh-r" as"program" bpick"*linux-x64.tar.gz" pick"mise/bin/mise"
+local _mise_os _mise_arch
+[[ "$OSTYPE" == "darwin"* ]] && _mise_os="macos" || _mise_os="linux"
+[[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]] && _mise_arch="arm64" || _mise_arch="x64"
+zinit ice from"gh-r" as"program" bpick"*${_mise_os}-${_mise_arch}.tar.gz" pick"mise/bin/mise"
 zinit light jdx/mise
 
 zinit ice from"gh-r" as"program"
