70d97945f5b496cd77e868b1a5ba09632ab09030
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2026-09-21 14:46:02 +0900

    feat(zsh): install tuicr from my fork (#72)

--- .zsh/10-tuicr.zsh
+++ .zsh/10-tuicr.zsh
@@ -3,4 +3,4 @@ local _tuicr_os _tuicr_arch
 [[ "$(uname -m)" == "arm64" || "$(uname -m)" == "aarch64" ]] && _tuicr_arch="aarch64" || _tuicr_arch="x86_64"
 
 zinit ice from"gh-r" as"program" bpick"tuicr-*-${_tuicr_arch}-${_tuicr_os}.tar.gz" pick"tuicr"
-zinit light agavra/tuicr
+zinit light shouichi/tuicr
